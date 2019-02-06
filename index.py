47cc08815e9698b04b1b60e16bc85aca05aaee4f
gamer154 <thegamer.one154@gmail.com>
2019-02-06 08:38:19 -0600

    first commit

---
+++ index.py
@@ -0,0 +1,23 @@
+import requests
+import time
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import render_template
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def test():
+    return  "Hello there my friend !!"
+	
+	
+if __name__ == "__main__":
+	port = int(os.getenv('PORT', 5000))
+	print("Starting app on port %d" % port)
+	app.run(debug=True, port=port, host='0.0.0.0')
