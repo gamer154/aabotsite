be5474eed3c3619229753ef067185d01841deaa0
gamer154 <thegamer.one154@gmail.com>
2019-02-06 12:38:25 -0600

    adding a way to trigger welcome event

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import random
 
 import json
 import os
@@ -18,8 +19,16 @@ def test():
 	
 @app.route('/home', methods=['GET'])
 def home():
+    triggerwelcome()
     return  render_template("home.html")
 
+def triggerwelcome():
+    #seId = random.randint(1,100)*50
+	triggerurl = "https://dialogflow.googleapis.com/v2/projects/aachatbot-81afa/agent/sessions/aa551b50-44c2-a6f8-9f46-7ff1cd7e3856:detectIntent"
+	data = {"queryInput":{"text":{"text":"Hello","languageCode":"en"}},"queryParams":{"timeZone":"America/Chicago"}}
+    data_json = json.dumps(data)
+    headers = {'Content-Type':'application/json', 'Authorization':'Bearer ya29.c.ElqoBq8wpD0R2ygLBssKhbKlR7L87_7JIpm15ukZ3ruW1AXyTGA3mZMORaIjYc1nZewvTXiyFYVZAr3wqYcX6Za_GsNGrNZgtJ4J290rnqk5VdOLteHh-fBhryk'}
+    response = requests.post(triggerurl, data=data_json, headers=headers)
 	
 if __name__ == "__main__":
 	port = int(os.getenv('PORT', 5000))
