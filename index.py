40be2e8d3d1f2ec89b8830edd15c129d3c60552d
gamer154 <thegamer.one154@gmail.com>
2019-02-06 12:46:51 -0600

    fixed indents

--- index.py
+++ index.py
@@ -24,8 +24,8 @@ def home():
 
 def triggerwelcome():
     #seId = random.randint(1,100)*50
-	triggerurl = "https://dialogflow.googleapis.com/v2/projects/aachatbot-81afa/agent/sessions/aa551b50-44c2-a6f8-9f46-7ff1cd7e3856:detectIntent"
-	data = {"queryInput":{"text":{"text":"Hello","languageCode":"en"}},"queryParams":{"timeZone":"America/Chicago"}}
+    triggerurl = "https://dialogflow.googleapis.com/v2/projects/aachatbot-81afa/agent/sessions/aa551b50-44c2-a6f8-9f46-7ff1cd7e3856:detectIntent"
+    data = {"queryInput":{"text":{"text":"Hello","languageCode":"en"}},"queryParams":{"timeZone":"America/Chicago"}}
     data_json = json.dumps(data)
     headers = {'Content-Type':'application/json', 'Authorization':'Bearer ya29.c.ElqoBq8wpD0R2ygLBssKhbKlR7L87_7JIpm15ukZ3ruW1AXyTGA3mZMORaIjYc1nZewvTXiyFYVZAr3wqYcX6Za_GsNGrNZgtJ4J290rnqk5VdOLteHh-fBhryk'}
     response = requests.post(triggerurl, data=data_json, headers=headers)
